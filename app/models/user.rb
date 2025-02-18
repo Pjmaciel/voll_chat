f704659ab43fe1f5f9da156515d7d0c26d4b306d
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-18 09:56:58 -0300

    refactor(Message): Adjusts Settings for Message Support

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+
+  has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message', foreign_key: 'recipient_id'
 end
