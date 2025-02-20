534bfa80c4e97b48dad7a03dccdcdc258f92228b
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-20 10:21:26 -0300

    fix(chat): corrects authentication Websocket

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,6 +1,7 @@
+# app/channels/chat_channel.rb
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "chat_#{params[:room]}"
+    stream_for current_user
   end
 
   def unsubscribed
@@ -8,12 +9,19 @@ class ChatChannel < ApplicationCable::Channel
   end
 
   def receive(data)
-    ActionCable.server.broadcast(
-      "chat_#{params[:room]}",
+    message = Message.create!(
+      content: data['content'],
+      sender: current_user,
+      recipient_id: data['recipient_id']
+    )
+
+    ChatChannel.broadcast_to(
+      message.recipient,
       {
-        message: data['message'],
-        user_id: current_user.id,
-        created_at: Time.current
+        id: message.id,
+        content: message.content,
+        sender: { id: current_user.id, email: current_user.email },
+        created_at: message.created_at
       }
     )
   end
